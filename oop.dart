1d44469d6348d73c873c857575ca4c1688a95f12
ahmedsalehalzabr <ahmedalzabr2@gmail.com>
2024-07-06 19:09:01 -1200

    video 5

--- oop.dart
+++ oop.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 void main(){
 Embloyee emp = Embloyee();
 print(emp.address);
@@ -7,6 +9,10 @@ Embloyee emp1 = Embloyee.ahmed(2,"ali","aden");
 print(emp1.address);
 print(emp1.name);
 print(emp1.id);
+
+Accuontant embloyees =Accuontant(1, "Ahmed", "developer", 25,12);
+embloyees.view();
+
 }
 
 
@@ -34,3 +40,32 @@ this.address=address;
 
 // constructor => special function فانكشن مالهاش داتا تايب
 
+
+// الوراثة
+class Embloyees {
+  int id;
+  String name;
+  String job;
+  int age;
+  Embloyees(this.id,this.name,this.job,this.age);
+
+  void view (){
+    print('id:$id');
+    print('age:$age');
+    print('job:$job');
+    print('name:$name');
+  }
+}
+
+
+class Accuontant extends Embloyees {
+  double salary;
+
+  Accuontant(int id, String name, String job, int age,this.salary):super(id,name,job,age);
+
+@override
+void view(){
+  super.view();
+  print('salary:$salary');
+}
+}
